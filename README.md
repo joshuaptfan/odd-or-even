e8eef15be33ac3a67428f3152cb2caa6465f8dc9
joshuaptfan <joshuaptfan@gmail.com>
2021-09-12 14:03:54 -0700

    Add solo mode

--- README.md
+++ README.md
@@ -1,38 +1,26 @@
 # Odd or Even
 
-A game of simple speed math and reflexes, played between 2 teams on a single touchscreen.
+A game of simple speed math and reflexes. Play solo or local versus.
 
 Web app: __https://oddoreven.app/__
 
-[Progressive Web App](https://developers.google.com/web/progressive-web-apps) &mdash; can be installed on Android/iOS for the best fullscreen experience.
-
-This game is free. If you enjoy it, consider making a small donation according to what you feel the game is worth.
-
-[![PayPal](images/paypal.svg)](https://paypal.me/joshuaptfan)
-
-<details>
-<summary>Table of Contents</summary>
-
-* [How to Play](#how-to-play)
-  * [Math Tips](#math-tips)
-  * [Ideas for Play](#ideas-for-play)
-* [Game Design](#game-design)
-  * [Conception](#conception)
-  * [Shared Buttons](#shared-buttons)
-  * [Perfectly Symmetrical, As All Things Should Be](#perfectly-symmetrical-as-all-things-should-be)
-  * [Fair Random](#fair-random)
-  * [Pacing](#pacing)
-</details>
+[Progressive Web App](https://developers.google.com/web/progressive-web-apps) &mdash; can be installed on Android/iOS for the best fullscreen experience. The UI will be larger, and the game won't exit fullscreen when switching back from another app.
 
 ## How to Play
 
-_Take the in-game tutorial for a shorter description of gameplay._
+### Solo
+
+Choose whether each math expression's result is odd or even before time runs out. The time limit decreases gradually as the game progresses.
+
+### Versus
+
+_Take the in-game tutorial for a briefer description of gameplay._
 
 There are 2 teams, __Odd__ and __Even__. Each math expression's result has a _parity_ of either __odd__ or __even__.
 
-There are 2 buttons in the center, &#9711; and &#10005;. If the result matches your team, press &#9711;. If it doesn't match your team, press &#10005;. Race against your opponent to press the correct button for your team.
+There are 2 buttons in the center, &#9711; and &#10005;. If the expression matches your team, press &#9711;. If it doesn't match your team, press &#10005;. Race against your opponent to press the correct button for your team.
 
-After each answer, the math expression changes. The game ends when one team gains a predetermined point lead over the other.
+The game ends when one team gains a preset point lead over the other.
 
 How the buttons work is that each turn, they both affect the score of the team whose parity matches the expression result, adding or subtracting 1 from that team's score.
 
@@ -52,7 +40,7 @@ It is not necessary to actually evaluate the expression to find out its [parity]
 * even &times; odd = even
 * odd &times; odd = odd
 
-Easy and normal difficulties only include addition and subtraction, so determining parity in those difficulties is only a matter of counting the number of odd numbers. __If there are an odd number of odd numbers, the result is odd.__
+Easy and normal difficulties only include addition and subtraction, so determining parity is only a matter of counting the number of odd numbers. __If there are an odd number of odd numbers, the result is odd.__
 
 Hard difficulty adds multiplication to the mix, forcing players to pay attention to the order of operations.
 
@@ -61,43 +49,13 @@ Hard difficulty adds multiplication to the mix, forcing players to pay attention
 * 2v2 &mdash; large touchscreen recommended
 * Drinking game &mdash; winner takes a drink
 
-## Game Design
+## What Inspired this Game
 
-### Conception
+In local multiplayer touchscreen games, there is a problem where the game has no way to be sure that any input originated from the correct player. So, I had an idea for a game that turns that problem into a feature. Originally, I wanted to make a game with 1 large button shared by all players, and that quickly evolved into the current nested 2-button design.
 
-In multiplayer games where players share a single touchscreen, there is a problem where the game has no way to be sure that any input originated from the correct player. So, I had an idea for a game that turns that problem into a feature. Originally, I wanted to make a game with 1 large button shared by all players, and that quickly evolved into the current nested 2-button design.
+This game is free. If you enjoy it, consider making a small donation according to what you feel the game is worth.
 
-### Shared Buttons
-
-The buttons are nested so that as a player is reaching for a button, it will be ambiguous to their opponent which button they will press, to prevent the strategy of just pressing the button opposite the one your opponent is reaching for. In this regard, there are alternative button designs that can also work, so long as the &#9711; and &#10005; tap areas are contiguous and they have a long shared border. I use a design that is rotationally symmetrical because initially I planned for this game to be playable by an arbitrary number of teams.
-
-A common request during testing was for each team to have their own buttons. I ultimately decided against implementing this for several reasons:
-
-1. The UI would be much more intuitive with a more conventional layout. However, the concept that both team's &#9711; and &#10005; buttons are linked together, i.e. pressing one &#9711; button also presses the other, would be proportionally _less_ intuitive.
-
-2. I wanted the UI to emphasize the versus aspect of the game. If the UI could be divided such that each team only needed to pay attention to their side, then it would effectively be 2 single-player games bolted together.
-
-3. Often, the motive for this request was rooted in the requester's personal discomfort with the possibility of making physical contact with another player when going for the smaller center button. However, one of the pillars of this game's design is to provoke conflict between players, up to and including that of a physical nature. (Yes, I have watched friends get into a wrestling match over this game. So worth it.) I determined that I should not make a compromise to satisfy a subset of players that would diminish the spirit of the game.
-
-### Perfectly Symmetrical, As All Things Should Be
-
-The game's symmetry makes it inherently balanced. Even the turns are simultaneous.
-
-Some players speculate that the Even team has an advantage because even expressions are easier to spot. Even if that is true, which is debatable, it wouldn't affect the game balance, because then both teams could play as if they were Even, except Odd could just press the "wrong" button each time.
-
-### Fair Random
-
-Each turn, the expression's parity is chosen first, then the expression is generated and coerced into that parity.
-
-The parity is chosen randomly, weighted towards the team that has been chosen fewer times previously. A fudge factor is thrown in so that no team is ever guaranteed to be chosen even if that team has never been chosen before.
-
-### Pacing
-
-In a game all about reaction speed, players need breaks at regular intervals from the core game mechanic to prevent mental exhaustion. I provide these with mandatory pauses between each turn and longer ones at the end of each game and the beginning of the next.
-
-The pause between turns is &frac34; of a second, which is the longest break I feel I can give players without giving them time to begin wondering when the next turn will start. The math expression for the previous turn remains on-screen during this period so that the loser has extra time to process and learn from it.
-
-Still, how long of a break each player needs is subjective and I observed that some players become mentally exhausted before they can finish even one game. For those players, I recommend playing with a lower score limit. There is no turn time limit, so that players can mutually set their own pace and take breaks at any time.
+[![PayPal](images/paypal.svg)](https://paypal.me/joshuaptfan)
 
 ## License
 
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+var solo = true;
 var ptDelta = (n => n === '\u221E' ? null : parseInt(n))(localStorage.ptDelta || 10);
-var difficulty = parseInt(localStorage.difficulty || 1);
+var difficulty = {
+	solo: parseInt(localStorage.difficulty || 1) % 3,
+	versus: Math.trunc(parseInt(localStorage.difficulty || 3) / 3)
+}
 var handicap = 0;
-var stats = [];
-var currPlayer = 0;
+var stats;
+var currParity = 0;
 var tutorialStep = null;
 var buzzerLock = false;
 var canWarnTouch = true;
@@ -11,11 +15,11 @@ var installPrompt;
 
 document.onreadystatechange = function () {
 	const home = document.querySelector('.home');
+	const setup = document.querySelector('.setup');
 	const game = document.querySelector('.game');
 	document.querySelector('.num-points').textContent = ptDelta;
 	document.querySelector('.incr-points[value="-1"]').disabled = ptDelta === 5 ? true : false;
 	document.querySelector('.incr-points[value="1"]').disabled = !ptDelta ? true : false;
-	document.querySelector('[name="difficulty"][value="' + difficulty + '"]').checked = true;
 	if (parseInt(localStorage.colorBlind))
 		game.classList.add('color-blind');
 
@@ -28,23 +32,29 @@ document.onreadystatechange = function () {
 		warnPortrait();
 	}, { passive: true });
 	home.addEventListener('mousemove', warnTouch);
-	document.querySelector('.buzzer').addEventListener('touchstart', e => {
-		buzz(e);
-	});
-	document.querySelector('.buzzer').addEventListener('click', e => {
-		buzz(e);
-	});
+	document.querySelector('.buzzer.solo').addEventListener('touchstart', e => buzz(e));
+	document.querySelector('.buzzer.solo').addEventListener('click', e => buzz(e));
+	document.querySelector('.buzzer.versus').addEventListener('touchstart', e => buzz(e));
+	document.querySelector('.buzzer.versus').addEventListener('click', e => buzz(e));
 	document.addEventListener('click', e => {
 		if (e.target.classList.item(0) === 'select-bg') {
 			e.target.style.display = 'none';
-			document.querySelector('.setup').classList.remove('blur');
+			setup.classList.remove('blur');
 			document.querySelector('.back-btn').classList.remove('blur');
 		}
 		if (!/BUTTON|LABEL/.test(e.target.tagName)) return;
 		switch (e.target.classList.item(0)) {
-			case 'play-btn':
+			case 'solo-btn':
+				solo = true;
+				setup.classList.add('solo');
+				game.classList.add('solo');
+				document.querySelector('[name="difficulty"][value="' + difficulty.solo + '"]').checked = true;
+				changePage('.home', '.setup', 'left');
+				break;
+			case 'versus-btn':
+				solo = false;
 				tutorialStep = null;
-				game.classList.remove('tutorial', 'mark-up', 'mark-dn');
+				document.querySelector('[name="difficulty"][value="' + difficulty.versus + '"]').checked = true;
 				changePage(home.classList.contains('activated') ? '.home' : '.game', '.setup', 'left');
 				break;
 			case 'tutorial-btn':
@@ -69,11 +79,11 @@ document.onreadystatechange = function () {
 				localStorage.ptDelta = ptDelta || '\u221E';
 				break;
 			case 'difficulty':
-				difficulty = parseInt(e.target.children[0].value);
-				localStorage.difficulty = difficulty;
+				difficulty[solo ? 'solo' : 'versus'] = parseInt(e.target.children[0].value);
+				localStorage.difficulty = difficulty.versus * 3 + difficulty.solo;
 				break;
 			case 'handicap-btn':
-				document.querySelector('.setup').classList.add('blur');
+				setup.classList.add('blur');
 				document.querySelector('.back-btn').classList.add('blur');
 				document.querySelector('.select-bg.handicap').style.display = 'block';
 				break;
@@ -118,23 +128,26 @@ document.onreadystatechange = function () {
 			case 'back-btn':
 				switch (document.querySelector('.activated').classList.item(1)) {
 					case 'setup':
+						game.classList.remove('solo');
 						changePage('.setup', '.home', 'right');
 						break;
 					case 'about':
 						changePage('.about', '.home', 'right');
 						break;
 					case 'game':
-						tutorialStep = null;
-						game.classList.remove('tutorial', 'mark-up', 'mark-dn');
 						changePage('.game', '.home', 'right');
 				}
 		}
 	});
 	document.addEventListener('animationend', e => {
-		if (e.target.classList.contains('deactivated'))
-			e.target.classList.remove('activated', 'deactivated', 'fly-out-left', 'fly-out-right');
-		else if (e.target.classList.contains('activated'))
+		if (e.target.classList.contains('deactivated')) {
+			e.target.classList.remove('solo', 'activated', 'deactivated', 'fly-out-left', 'fly-out-right');
+			if (e.target.classList.contains('tutorial'))
+				game.classList.remove('tutorial', 'mark-up', 'mark-dn');
+		} else if (e.target.classList.contains('activated'))
 			e.target.classList.remove('fly-in-left', 'fly-in-right');
+		else if (e.target.classList.contains('running'))
+			score(null, null);
 	});
 
 	fullscreen: if (!window.matchMedia('(display-mode: fullscreen)').matches && !navigator.standalone) {
@@ -152,8 +165,7 @@ document.onreadystatechange = function () {
 		about.classList.add('install', /^iP(hone|[ao]d)/.test(navigator.platform) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) ? 'ios' : 'android');
 		if (!document.fullscreenEnabled && !document.webkitFullscreenEnabled) break fullscreen;
 		home.querySelector('.fullscreen-btn').style.display = 'block';
-		game.querySelector('.fullscreen-btn').style.display = 'block';
-		game.querySelector('.color-blind-btn').style.marginTop = '119rem';
+		game.querySelector('.settings').classList.add('fullscreen-enabled');
 	}
 
 	if (!navigator.share)
@@ -217,9 +229,10 @@ document.onreadystatechange = function () {
 };
 
 function initTutorial() {
+	solo = false;
 	tutorialStep = 1;
 	document.querySelector('.game').classList.add('tutorial');
-	document.querySelector('.buzzer').className = 'buzzer';
+	document.querySelector('.buzzer.versus').className = 'buzzer versus';
 	setTutorialStep(0);
 	initPlayers();
 	genExpression();
@@ -229,8 +242,8 @@ function setTutorialStep(n) {
 	tutorialStep += n;
 	document.querySelector('.game').dataset.step = tutorials[tutorialStep - 1].s;
 	document.querySelector('.game > progress').value = tutorialStep / tutorials.length;
-	document.querySelector('.prev-btn').disabled = tutorialStep === 1 ? true : false;
-	document.querySelector('.next-btn').disabled = tutorialStep === tutorials.length ? true : false;
+	document.querySelector('.prev-btn').disabled = tutorialStep === 1;
+	document.querySelector('.next-btn').disabled = tutorialStep === tutorials.length;
 	document.querySelectorAll('.team').forEach(el => {
 		el.dataset.text = tutorials[tutorialStep - 1].t;
 	});
@@ -239,11 +252,11 @@ function setTutorialStep(n) {
 function initPlayers() {
 	const side = Math.trunc(Math.random() * 2);
 	const score = (s => handicap > 0 ? [s, 0] : [0, s])(Math.abs(handicap) * (ptDelta || 100));
-	document.querySelector('.up > .player').textContent = side ? 'Even' : 'Odd';
+	document.querySelector('.up > .name').textContent = side ? 'Even' : 'Odd';
 	let point = document.querySelector('.up > .point');
 	point.id = 'pt' + (side ? 0 : 1);
 	point.textContent = score[0];
-	document.querySelector('.dn > .player').textContent = side ? 'Odd' : 'Even';
+	document.querySelector('.dn > .name').textContent = side ? 'Odd' : 'Even';
 	point = document.querySelector('.dn > .point');
 	point.id = 'pt' + (side ? 1 : 0);
 	point.textContent = score[1];
@@ -256,12 +269,25 @@ function initPlayers() {
 }
 
 function initGame() {
-	initPlayers();
+	const timer = document.querySelector('.timer.solo');
+	const buzzer = document.querySelector('.buzzer' + (solo ? '.solo' : '.versus'));
 	buzzerLock = true;
-	const buzzer = document.querySelector('.buzzer');
-	buzzer.className = 'buzzer';
-	void buzzer.offsetWidth;
-	buzzer.classList.add('countdown');
+	if (solo) {
+		stats = { pt: 0, t: 0, l: 0 };
+		document.querySelector('.score.solo > .point').textContent = 0;
+		while (stats.l < 3)
+			addLives(1);
+		setTimer(0);
+		timer.classList.remove('countdown', 'running');
+		void timer.offsetWidth;
+		timer.classList.add('countdown');
+		buzzer.className = 'buzzer solo';
+	} else {
+		initPlayers();
+		buzzer.className = 'buzzer versus';
+		void buzzer.offsetWidth;
+		buzzer.classList.add('countdown');
+	}
 	const countdown = countdowns[Math.trunc(Math.random() * countdowns.length)];
 	let i = 0;
 	setExpression(countdown[0]);
@@ -272,64 +298,109 @@ function initGame() {
 		else {
 			clearInterval(interval);
 			buzzerLock = false;
-			buzzer.classList.remove('countdown');
+			if (solo) {
+				timer.classList.remove('countdown');
+				void timer.offsetWidth;
+				timer.classList.add('running');
+			} else
+				buzzer.classList.remove('countdown');
 			genExpression();
 		}
 	}, 1000);
 }
 
+function setTimer(t) {
+	const timer = document.querySelector('.timer.solo');
+	timer.style.width = (.98 ** t + 1) * 15 + 'rem';
+	timer.style.animationDuration = .98 ** t + 1 + 's';
+}
+
+function addLives(n) {
+	document.querySelector('.lives.solo > img:nth-of-type(' + Math.min(parseInt(stats.l + 1), 3) + ')').src = 'images/heart.svg#h' + parseInt(n < 0 ? 0 : 8);
+	stats.l = Math.max(0, Math.min(stats.l + n, 3));
+	if (stats.l === 3) return;
+	document.querySelector('.lives.solo > img:nth-of-type(' + parseInt(stats.l + 1) + ')').src = 'images/heart.svg#h' + parseInt(stats.l % 1 * 8);
+}
+
 function score(e, val) {
-	stats[currPlayer].pt += val;
-	stats[currPlayer].t++;
+	if (solo) {
+		const timer = document.querySelector('.timer.solo');
+		const correct = val === currParity;
+		stats.pt += correct ? 1 : 0;
 
-	const point = document.querySelector('#pt' + currPlayer);
-	point.textContent = stats[currPlayer].pt;
-	point.dataset.value = stats[currPlayer].pt;
-	point.classList.remove('flash', 'neg', 'pos');
-	void point.offsetWidth;
-	point.classList.add('flash', val < 0 ? 'neg' : 'pos');
+		addLives(correct ? .125 : -1);
+		document.querySelector('.score.solo > .point').textContent = stats.pt;
+		timer.classList.remove('running');
+		void timer.offsetWidth;
+		if (stats.l === 0) {
+			setExpression('GAME OVER');
+			document.querySelector('.buzzer.solo').classList.add('game-over');
+			return;
+		}
+		setTimer(++stats.t);
+		timer.classList.add('running');
+		genExpression();
+		if (!e) return;
+		e.target.classList.remove('neg', 'pos');
+		void e.target.offsetWidth;
+		e.target.classList.add(correct ? 'pos' : 'neg');
+	} else {
+		stats[currParity].pt += val;
+		stats[currParity].t++;
 
-	buzzerLock = true;
-	const buzzer = document.querySelector('.buzzer');
-	document.querySelector('.lead').style.top = ((stats[0].pt - stats[1].pt) / (ptDelta || 100)) * (stats[0].d === 'up' ? -50 : 50) + 50 + '%';
-	if (tutorialStep)
-		document.querySelector('.game').classList.remove('mark-up', 'mark-dn');
-	else if (Math.abs(stats[0].pt - stats[1].pt) === ptDelta) {
-		setExpression((stats[0].pt > stats[1].pt ? 'EVEN' : 'ODD') + ' WINS');
-		buzzer.classList.add('win');
+		const point = document.querySelector('#pt' + currParity);
+		point.textContent = stats[currParity].pt;
+		point.dataset.value = stats[currParity].pt;
+		point.classList.remove('flash', 'neg', 'pos');
+		void point.offsetWidth;
+		point.classList.add('flash', val < 0 ? 'neg' : 'pos');
+
+		buzzerLock = true;
+		const buzzer = document.querySelector('.buzzer.versus');
+		document.querySelector('.lead').style.top = ((stats[0].pt - stats[1].pt) / (ptDelta || 100)) * (stats[0].d === 'up' ? -50 : 50) + 50 + '%';
+		if (tutorialStep)
+			document.querySelector('.game').classList.remove('mark-up', 'mark-dn');
+		else if (Math.abs(stats[0].pt - stats[1].pt) === ptDelta) {
+			setExpression((stats[0].pt > stats[1].pt ? 'EVEN' : 'ODD') + ' WINS');
+			buzzer.classList.add('game-over');
+			setTimeout(() => {
+				buzzerLock = false;
+			}, 2000);
+			return;
+		}
+		if (val > 0)
+			buzzer.classList.add('pos');
+		buzzer.classList.add('wave-' + stats[currParity].d);
+		e.target.classList.add('active');
 		setTimeout(() => {
 			buzzerLock = false;
-		}, 2000);
-		return;
+			buzzer.className = 'buzzer versus';
+			e.target.classList.remove('active');
+			genExpression();
+		}, 750);
 	}
-	if (val > 0)
-		buzzer.classList.add('pos');
-	buzzer.classList.add('wave-' + stats[currPlayer].d);
-	e.target.classList.add('active');
-	setTimeout(() => {
-		buzzerLock = false;
-		buzzer.className = 'buzzer';
-		e.target.classList.remove('active');
-		genExpression();
-	}, 750);
 }
 
 function genExpression() {
-	// Choose player via random weighted by turns
-	const sSort = [...stats].sort((a, b) => a.t - b.t);
-	let rand = Math.random() * stats.reduce((s, v) => s + v.t + 7, 0);
-	for (let i = 0; i < 2; i++) {
-		rand -= sSort[i].t + 7;
-		if (rand < 0) {
-			currPlayer = 1 - sSort[i].p;
-			break;
+	if (solo)
+		currParity = Math.trunc(Math.random() * 2);
+	else {
+		// Choose player via random weighted by turns
+		const sSort = [...stats].sort((a, b) => a.t - b.t);
+		let rand = Math.random() * stats.reduce((s, v) => s + v.t + 7, 0);
+		for (let i = 0; i < 2; i++) {
+			rand -= sSort[i].t + 7;
+			if (rand < 0) {
+				currParity = 1 - sSort[i].p;
+				break;
+			}
 		}
+		if (tutorialStep)
+			document.querySelector('.game').classList.add('mark-' + (stats[currParity].d));
 	}
-	if (tutorialStep)
-		document.querySelector('.game').classList.add('mark-' + (stats[currPlayer].d));
 
 	// Generate expression
-	const diff = difficulties[tutorialStep ? 0 : difficulty];
+	const diff = difficulties[tutorialStep ? 0 : difficulty[solo ? 'solo' : 'versus']];
 	let ex = [];
 	const maxLen = Math.trunc(Math.random() * (diff.len - 1)) + 2;
 	let remLen = maxLen;
@@ -358,7 +429,7 @@ function genExpression() {
 	}
 	// Reduce additions
 	// If parity does not match current player, flip parity
-	flip: if (exP.reduce((p, v) => p + v.par & 1, 0) !== currPlayer) {
+	flip: if (exP.reduce((p, v) => p + v.par & 1, 0) !== currParity) {
 		// Flip additive component if exists
 		for (let i = 0; i < ex.length; i++) {
 			if (ex[i].opr === 3 || ex[i + 1] && ex[i + 1].opr === 3) continue;
@@ -381,7 +452,5 @@ function genExpression() {
 }
 
 function setExpression(ex) {
-	document.querySelectorAll('.expression').forEach(el => {
-		el.innerHTML = ex;
-	});
+	document.querySelectorAll('.expression' + (solo ? '.solo' : '.versus')).forEach(el => el.innerHTML = ex);
 }
